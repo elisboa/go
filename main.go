ca1315bd5549b27d5de84d19b2e202ffddbb4357
Eduardo Lisboa <eduardo.lisboa@gmail.com>
2021-11-19 23:21:18 -0300

    :rocket: elisboa 2021.11.19-23.21 - main.go - var types

--- main.go
+++ main.go
@@ -16,5 +16,40 @@ func main() {
     // package called Println() in order to print
     // out `Hello World`
     fmt.Println("Hello World")
+
+    // integer overflow test
+    var myint int8
+    for i := 0; i < 129; i++ {
+        myint += 1
+    }
+    fmt.Println(myint) // prints out -127
+
+
+    // Simple bool verification
+    var amazing bool
+    amazing = true
+    if amazing {
+      fmt.Println("Subscribe to our channel!")
+    }
+
+    // Testing 'If' statement 
+    var isTrue bool = true
+    var isFalse bool = true
+    // AND
+    if isTrue && isFalse {
+      fmt.Println("Both Conditions need to be True")
+    }
+    // OR - not exclusive
+    if isTrue || isFalse {
+      fmt.Println("Only one condition needs to be True")
+    }
+
+    var myName string
+    myName = "Eduardo Lisboa"
+    fmt.Println("My name is", myName)
+
+    const meaningOfLife = 42
+    fmt.Println("The meaning of life is", meaningOfLife)
+
 }
 
